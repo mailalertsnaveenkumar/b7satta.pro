1a1f8e595ff8c546be68cf1f8f7efc1ac884998f
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2025-12-24 23:16:05 +0530

    Update Luckynumber.jsx

--- src/assets/components/Luckynumber.jsx
+++ src/assets/components/Luckynumber.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Navigation, Pagination, Scrollbar, b71y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
@@ -122,3 +122,4 @@ const Luckynumber = () => {
 };
 
 export default Luckynumber;
+
